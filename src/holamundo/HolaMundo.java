d67054df9337f6bc68db5e709173efaa88da4261
alnazareth <36893691+alnazareth@users.noreply.github.com>
2024-05-18 13:51:58 -0400

    initial update

---
+++ src/holamundo/HolaMundo.java
@@ -0,0 +1,25 @@
+
+package holamundo;
+
+
+public class HolaMundo {
+
+ 
+    public static void main(String[] args) {
+       
+      //condicionales
+        int num1=4;
+        int num2=5;
+        if(num1>num2){
+            System.out.println("Num1 "+num1+" es mayor que Num2 "+num2);
+        }else if (num1<num2){
+            System.out.println("Num2 "+num2+" es mayor que Num1 "+num1);
+        }else{
+            System.out.println("num1 y num2 son iguales ");
+        }
+        
+        
+
+    }
+    
+}
